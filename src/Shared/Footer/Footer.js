c66246265ff58816194efd621337e5fcf03992df
akram hoque <oriniusakram511@gmail.com>
2022-08-27 20:29:57 +0600

    first commit

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+       
+        
+            <div className="footer-style">
+                <p> Copyright <span dangerouslySetInnerHTML={{ "__html": "&copy;" }}></span> All the Reserved to Oraganic Fruits 2022</p>
+            </div>
+            
+       
+    );
+};
+
+export default Footer;
